89d81017ed04e32dace20063b39568ed4966fdbb
Mariah Chavez-Graham <mgraham21@cnm.edu>
2020-02-18 15:57:36 -0700

    Need to fix enum on Agency

--- src/main/java/edu/cnm/deepdive/server/model/entity/Agency.java
+++ src/main/java/edu/cnm/deepdive/server/model/entity/Agency.java
@@ -1,5 +1,49 @@
 package edu.cnm.deepdive.server.model.entity;
 
+
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Table(
+    indexes = {
+        @Index(columnList = "agency_id")
+    }
+)
+
 public class Agency {
+
+  //Entity Elements
+
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid", strategy = "uuid2")
+  @Column(name = "agency_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+      nullable = false, updatable = false)
+  private UUID id;
+
+  @Enumerated(EnumType.STRING)
+  @Column
+  private AgencyType type;
+
+  //Foreign Keys
+
+  @OneToOne(fetch = FetchType.EAGER,
+      cascade = (CascadeType.DETACH, CascadeType.MERGE,  ))
+
 
 }
